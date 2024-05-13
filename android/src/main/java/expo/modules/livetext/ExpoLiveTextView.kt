d78c8e54f1598d08050911e9380d505f1b28224b
jfmoe <34602628+jfmoe@users.noreply.github.com>
2024-05-13 14:34:47 +0800

    build: remove android files

--- android/src/main/java/expo/modules/livetext/ExpoLiveTextView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.livetext
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoLiveTextView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
